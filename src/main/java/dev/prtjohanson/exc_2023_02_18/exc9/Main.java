4b669d7e31bb5b7d5df44ead037c745b5fe9ba15
prtjohanson <pjohanson@verticalscope.com>
2023-02-18 11:59:18 +0200

    Exercise 9

--- src/main/java/dev/prtjohanson/exc_2023_02_18/exc9/Main.java
+++ src/main/java/dev/prtjohanson/exc_2023_02_18/exc9/Main.java
@@ -3,10 +3,20 @@ package dev.prtjohanson.exc_2023_02_18.exc9;
 import dev.prtjohanson.exc_2023_02_18.exc7.Employee;
 import dev.prtjohanson.exc_2023_02_18.exc7.EmployeeListBuilder;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         final ArrayList<Employee> employees = new EmployeeListBuilder(10).getEmployees();
+        Optional<Employee> maxSalaryEmployee = employees.stream().max(Comparator.comparing(Employee::getSalary));
+
+        if (maxSalaryEmployee.isPresent()) {
+            System.out.println(maxSalaryEmployee);
+        } else {
+            System.out.println("No salary employee with max salary found!");
+        }
     }
 }
